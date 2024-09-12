372c793743cdbc7c6613c8149024f301b0c31f4c
‪Aqel Mohammed‬‏ <aqelmoh77@gmail.com>
2024-09-12 21:09:58 +0300

    Initial commit

--- countriesinfo/build.gradle.kts
+++ countriesinfo/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ak.countriesinfo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 24
@@ -36,7 +36,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
